5fdcc41f287df004dedb179942151603bd5f38d4
Atomer <1727165282@qq.com>
2016-08-31 17:32:08 +0800

    加入命令行支持，可以输出一些信息

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 ﻿// Add lib we need
+//CxImage
 #pragma comment(lib,"lib/cximage.lib")
 #pragma comment(lib,"lib/jasper.lib")
 #pragma comment(lib,"lib/jbig.lib")
@@ -10,11 +11,17 @@
 #pragma comment(lib,"lib/Tiff.lib")
 #pragma comment(lib,"lib/zlib.lib")
 
+//command line processing, getopt
+#pragma comment(lib,"lib/getopt.lib")
+#pragma comment(lib,"lib/getoptd.lib")
+
 
 #include <iostream>
 #include <string>
 #include "ximage.h"
 #include <math.h>
+#include "getopt.h"
+#include <stdlib.h>
 
 //we need to define UNICODE to use CxImage libarary
 #ifndef UNICODE
@@ -114,16 +121,19 @@ float** gaussianFilterCreator(float sigma, int size)
     }
     return f;
 }
-// for debug usage
-// void dispMatrix(float **im, size_t sx, size_t sy) {
-// std::cout.precision(10);
-// for (size_t i = 0; i < sy; i++) {
-// for (size_t  j = 0; j < sx; j++) {
-// std::cout  << im[i][j] << " ";
-// }
-// std::cout << std::endl;
-// }
-// }
+
+//for debug usage
+//void dispMatrix(float **im, size_t sx, size_t sy)
+//{
+//    std::cout.precision(10);
+//    for (size_t i = 0; i < sy; i++) {
+//        for (size_t  j = 0; j < sx; j++) {
+//            std::cout  << im[i][j] << " ";
+//        }
+//        std::cout << std::endl;
+//    }
+//}
+
 typedef struct pos {
     size_t x, y;
 } position;
@@ -256,20 +266,43 @@ float** htSasanMethod(float **im, size_t imx, size_t imy, float **filter, size_t
     return biImg;
 }
 
-
-int main()
+void halftoneProcess(char *filename)
 {
     CxImage image;
     //divide the image into small blocks so we can process it in parallel by adding OpenMP support
     size_t blockSize = 32;
     size_t fltSize = 11;
     float **gausFilter = gaussianFilterCreator(1.3f, 11);
+    char drive[_MAX_DRIVE];
+    char dir[_MAX_DIR];
+    char fname[_MAX_FNAME];
+    char ext[_MAX_EXT];
+    //获取文件扩展名
+    TCHAR wcFilename[100];
+    _splitpath(filename, drive, dir, fname, ext);
+    strupr(ext);
+    uint32_t format = 0U;
+    if (0 == strcmp(ext, "BMP")) {
+        format = CXIMAGE_SUPPORT_BMP;
+    } else if (0 == strcmp(ext, "JPG")) {
+        format = CXIMAGE_SUPPORT_JPG;
+    } else if (0 == strcmp(ext, "PNG")) {
+        format = CXIMAGE_SUPPORT_PNG;
+    }
+    //考虑到MultiByte的情况
+#ifdef UNICODE
+    MultiByteToWideChar(CP_ACP, 0, filename, -1, wcFilename, 100);
+#else
+    strcpy(file, wcFilename);
+#endif
+
     // _T() is necessary if we need UNICODE support
-    if (image.Load(_T("lena.jpg") , CXIMAGE_SUPPORT_JPG)) {
+    if (image.Load(wcFilename, format)) {
         // To simplify the problem ,convert the image to 8 bits gray scale
         image.GrayScale();
         size_t width = image.GetWidth();
         size_t height = image.GetHeight();
+        printf("\nimage size(width x height): %upixels x %upixels", width, height);
         size_t xBlocks = (width + blockSize - 1) / blockSize;
         size_t yBlocks = (height + blockSize - 1) / blockSize;
         size_t padImg_x = blockSize * xBlocks;
@@ -284,13 +317,13 @@ int main()
             }
         }
         //padding image
-        for(size_t x = width; x < padImg_x; x++) {
-            for(size_t y = 0; y < height; y++) {
+        for (size_t x = width; x < padImg_x; x++) {
+            for (size_t y = 0; y < height; y++) {
                 imageData[y][x] = imageData[y][width - 1];
             }
         }
-        for(size_t y = height; y < padImg_y; y++) {
-            for(size_t x = 0; x < padImg_x; x++) {
+        for (size_t y = height; y < padImg_y; y++) {
+            for (size_t x = 0; x < padImg_x; x++) {
                 imageData[y][x] = imageData[height - 1][x];
             }
         }
@@ -298,6 +331,7 @@ int main()
         //halftone image
         float **htImageBlackIsZero;
         float **htImageWhiteIsZero;
+        //处理的时候,控制少数点的位置,可以块边缘处的缺陷
         new2dInitZero(padImg_x, padImg_y, &htImageBlackIsZero);
         new2dInitZero(padImg_x, padImg_y, &htImageWhiteIsZero);
         //计时函数
@@ -306,8 +340,11 @@ int main()
         DWORD dwTimes;
         dwStart = GetTickCount();
         /////////////////////////////////////////////////////////////////////
-        size_t phase_coef[4][2] = {0, 0, 1, 0, 0, 1, 1, 1};
-        for(size_t p = 0; p < 4; p++) {
+        //显示完成百分比
+        printf("\nprocessing image:\t0%% finish");
+        size_t blockCount = 0;
+        size_t phase_coef[4][2] = { 0, 0, 1, 0, 0, 1, 1, 1 };
+        for (size_t p = 0; p < 4; p++) {
             uint8_t edgeMask = 0x00;
             size_t phase_x;
             size_t phase_y;
@@ -315,71 +352,75 @@ int main()
             phase_y = (yBlocks + 1 - phase_coef[p][1]) / 2;
             float **fltHtImageBlackIsZero = imfilter(htImageBlackIsZero, padImg_x, padImg_y, gausFilter, fltSize, fltSize);
             float **fltHtImageWhiteIsZero = imfilter(htImageWhiteIsZero, padImg_x, padImg_y, gausFilter, fltSize, fltSize);
-            for(size_t i = 0; i < phase_y; i++) {
-                for(size_t j = 0; j < phase_x; j++) {
+
+            for (size_t i = 0; i < phase_y; i++) {
+                //printf("%c%c", 0xa8, 0x80);
+                for (size_t j = 0; j < phase_x; j++) {
+                    blockCount++;
                     size_t blockIdx_x = 2 * j + phase_coef[p][0];
                     size_t blockIdx_y = 2 * i + phase_coef[p][1];
                     size_t xc = blockSize * blockIdx_x;
                     size_t yc = blockSize * blockIdx_y;
-                    switch(p) {
+                    switch (p) {
                     case 0:
                         edgeMask = 0xff;
                         break;
                     case 1:
                         edgeMask = 0xf3;
-                        if(blockIdx_x == xBlocks - 1) {
+                        if (blockIdx_x == xBlocks - 1) {
                             edgeMask |= 0x08;
                         }
                         break;
                     case 2:
                         edgeMask = 0x0c;
-                        if(blockIdx_x == xBlocks - 1) {
+                        if (blockIdx_x == xBlocks - 1) {
                             edgeMask |= 0xc0;
                         }
-                        if(blockIdx_x == 0) {
+                        if (blockIdx_x == 0) {
                             edgeMask |= 0x30;
                         }
-                        if(blockIdx_y == yBlocks - 1) {
+                        if (blockIdx_y == yBlocks - 1) {
                             edgeMask |= 0xa2;
                         }
                         break;
                     case 3:
                         edgeMask = 0x00;
-                        if(blockIdx_x == xBlocks - 1) {
+                        if (blockIdx_x == xBlocks - 1) {
                             edgeMask |= 0xc8;
                         }
-                        if(blockIdx_y == yBlocks - 1) {
+                        if (blockIdx_y == yBlocks - 1) {
                             edgeMask |= 0xa2;
                         }
                         break;
                     }
                     size_t halfDots = blockSize * blockSize / 2;
                     bool blackIsMinor;
+                    //这里ndots使用size_t类型会出错，float与size_t的转换还是要小心
                     float ndots = 0;
-                    for(size_t i = 0; i < blockSize; i++) {
-                        for(size_t j = 0; j < blockSize; j++) {
+                    for (size_t i = 0; i < blockSize; i++) {
+                        for (size_t j = 0; j < blockSize; j++) {
                             ndots += imageData[yc + i][xc + j];
                         }
                     }
                     blackIsMinor = ndots > halfDots;
                     float** blockImage;
                     new2dInitZero(blockSize, blockSize, &blockImage);
-                    for(size_t m = 0; m < blockSize; m++) {
-                        for(size_t n = 0; n < blockSize; n++) {
-                            if(blackIsMinor) {
+                    for (size_t m = 0; m < blockSize; m++) {
+                        for (size_t n = 0; n < blockSize; n++) {
+                            if (blackIsMinor) {
                                 blockImage[m][n] = 1 - blurImageData[yc + m][xc + n] - fltHtImageWhiteIsZero[yc + m][xc + n];
                             } else {
                                 blockImage[m][n] = blurImageData[yc + m][xc + n] - fltHtImageBlackIsZero[yc + m][xc + n];
                             }
                         }
                     }
-                    if(blackIsMinor) {
+                    if (blackIsMinor) {
                         ndots = blockSize * blockSize - ndots;
                     }
                     float **res = htSasanMethod(blockImage, blockSize, blockSize, gausFilter, 11, 11, edgeMask, ndots);
-                    for(size_t m = 0; m < blockSize; m++) {
-                        for(size_t n = 0; n < blockSize; n++) {
-                            if(blackIsMinor) {
+                    for (size_t m = 0; m < blockSize; m++) {
+                        for (size_t n = 0; n < blockSize; n++) {
+                            if (blackIsMinor) {
                                 htImageBlackIsZero[yc + m][xc + n] = 1 - res[m][n];
                                 htImageWhiteIsZero[yc + m][xc + n] = res[m][n];
                             } else {
@@ -390,43 +431,72 @@ int main()
                     }
                     del2d(blockSize, blockSize, blockImage);
                     del2d(blockSize, blockSize, res);
+                    float percent = blockCount * 100.0 / (xBlocks * yBlocks);
+                    //显示完成百分比
+                    printf("\b\b\b\b\b\b\b\b\b\b\b\b\b%05.2f%% finish", percent);
+                    fflush(stdout);
                 }
             }
             del2d(padImg_x, padImg_y, fltHtImageBlackIsZero);
             del2d(padImg_x, padImg_y, fltHtImageWhiteIsZero);
         }
         //////////////////////////////////////////////////
+        //printf("|done!\n");
         dwEnd = GetTickCount();
         dwTimes = dwEnd - dwStart;
-        printf("加网lena图用时 %ld ms\n", dwTimes);
+        printf("\nprocessing time: %ld ms\n", dwTimes);
+
         uint8_t **htImageBlackIsZeroByte;
         new2dInitZero(padImg_x, padImg_y, &htImageBlackIsZeroByte);
-        for(size_t i = 0; i < padImg_y; i++) {
-            for(size_t j = 0; j < padImg_x; j++) {
+        for (size_t i = 0; i < padImg_y; i++) {
+            for (size_t j = 0; j < padImg_x; j++) {
                 htImageBlackIsZeroByte[i][j] = htImageBlackIsZero[i][j] * 255;
             }
         }
+
         CxImage outputImg;
         outputImg.CreateFromMatrix(htImageBlackIsZeroByte, width, height, 8, padImg_x, NULL);
-        outputImg.Save(_T("ht_lena.bmp"), CXIMAGE_SUPPORT_BMP);
+        strcat(fname, "##ht");
+        char generatedFileName[_MAX_PATH];
+        TCHAR wcGeneratedFileName[_MAX_PATH];
+        _makepath(generatedFileName, drive, dir, fname, ".bmp");
+#ifdef UNICODE
+        MultiByteToWideChar(CP_ACP, 0, generatedFileName, -1, wcGeneratedFileName , 100);
+#else
+        strcpy(file, filename);
+#endif
+        outputImg.Save(wcGeneratedFileName, CXIMAGE_SUPPORT_BMP);
+        printf("File Generated: %s.bmp\n", fname);
         del2d(11, 11, gausFilter);
         del2d(padImg_x, padImg_y, htImageBlackIsZero);
         del2d(padImg_x, padImg_y, htImageWhiteIsZero);
         del2d(padImg_x, padImg_y, htImageBlackIsZeroByte);
         del2d(padImg_x, padImg_y, imageData);
-        std::cout << "done\n";
-        getchar();
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
+int main(int argc, char**argv)
+{
+    int ch;
+    size_t l = 0;
+    char filename[_MAX_PATH];
+    //处理命令行参数
+    while ((ch = getopt(argc, argv, "f:h")) != -1) {
+        switch (ch) {
+        case 'h':
+            printf("\n");
+            printf("                    *** Sasan Halftoning Method ***                   \n");
+            printf("-f image filename                                                     \n");
+            printf("-h display this help message                                          \n");
+            goto exit;
+            break;
+        case 'f':
+            l = strlen(optarg);
+            strcpy(filename, optarg);
+            break;
+        }
+    }
+    halftoneProcess(filename);
+exit:
+    return 0;
+}
